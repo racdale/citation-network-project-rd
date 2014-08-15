549705270b8b3cd6e931d437260a55dab1b84424
racdale@gmail.com <racdale@gmail.com>
2014-08-14 23:53:04 -0700

    Update

--- textmodel2.R
+++ textmodel2.R
@@ -19,7 +19,7 @@ load('tXd.Rdata')
 library(irlba)
 S <<- irlba(tXd.mat,nu=25,nv=25) # very slow beyond 30 or so
 
-jids = as.numeric(names(sort(table(title$JID),decreasing=T)[30:60])) # get the most common journals
+jids = as.numeric(names(sort(table(title$JID),decreasing=T)[30:70])) # get the most common journals
 
 j_auth_cos = data.frame()
 
@@ -46,13 +46,39 @@ system.time(for (jid in jids) {
   }
 });
 
-jcosagg = aggregate(j_journ_cos[j_journ_cos$n>10,]$cos,by=list(j_journ_cos[j_journ_cos$n>10,]$jid),mean) # only journals with n > 10 articles in the results
+jcosagg = aggregate(j_journ_cos[j_journ_cos$n>4,]$cos,by=list(j_journ_cos[j_journ_cos$n>4,]$jid),mean) # only journals with n > 10 articles in the results
 jcosagg = sort(jcosagg$x,index.return=T)
 plot(jcosagg$x,col='white',xlim=c(0,length(jids))) # plot names of journals ranked with y = cosine (higher = more consistent authorship content)
 text(1:length(jcosagg$ix)+1,jcosagg$x,labels=journal[jids,]$name,cex=.65)
 
-plot(acosagg$x,jcosagg$x,col='white',xlim=c(0.1,.4),ylim=c(0,.4))
+plot(acosagg$x,jcosagg$x,col='black')
+points(acosagg$x[6],jcosagg$x[6],col=rgb(0,.5,.2),lwd=10)
 text(acosagg$x,jcosagg$x,labels=journal[jids,]$name,cex=.65)
+
+
+j_vect_cos = data.frame()
+
+for (jid in jids) {
+  print(paste('author journal vector processing: ',jid,journal[jid,]$name))
+  artids = which(title$JID==jid)
+  for (j in artids) { # loop through all articles for this journal
+    auths = comb$AID[comb$TID==j] # get the authors of this title
+    auths = auths[author[auths,]$name!=""]
+    authtitles = unique(comb$TID[comb$AID %in% auths & comb$TID!=j])
+    journs = title[title$TID %in% authtitles,]$JID
+    journs2 = c()
+    for (auth in auths) {
+      authtitles = unique(comb$TID[comb$AID ==auth & comb$TID!=j])
+      journs2 = c(journs2,unique(title[title$TID %in% authtitles,]$JID))
+    }        
+  }      
+  res = length(unique(journs2))/length(journs2)
+  j_vect_cos = rbind(j_vect_cos,data.frame(jid=jid,cos=res))
+}
+
+
+
+
 
 
 
